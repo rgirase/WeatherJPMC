c6caccbb387109d2b858c2bd6d1b435c1cfc3b32
rahulgirase <rgirase1991@gmail.com>
2023-05-09 22:50:44 -0500

    optimize imports

--- app/src/main/java/com/rng/weatherappjpmc/MainActivity.kt
+++ app/src/main/java/com/rng/weatherappjpmc/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.rng.weatherappjpmc
 
 import android.Manifest
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,28 +8,15 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
-import androidx.lifecycle.lifecycleScope
-import com.rng.weatherappjpmc.data.model.lastLocation
 import com.rng.weatherappjpmc.presentation.viewmodel.WeatherViewModel
 import com.rng.weatherappjpmc.presentation.weather_home_screen.WeatherHomeScreen
 import com.rng.weatherappjpmc.ui.theme.DarkBlue
 import com.rng.weatherappjpmc.ui.theme.WeatherAppJPMCTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
